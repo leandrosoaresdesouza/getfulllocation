5be78d94a50a700bc10899ee418b9403ec64ed75
Leandro <lsoares199658@gmail.com>
2021-03-13 18:06:27 -0300

    'location'

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ function getPosition() {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
+    fetch("https://perguntea2020.herokuapp.com/mail", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        latitude,
+        longitude,
+      }),
+    }).then((response) => {
+      console.log("response", response);
+    });
+
     document.getElementById("latitude").innerHTML = "latitude" + latitude;
     document.getElementById("longitude").innerHTML = "longitude" + longitude;
-
-    document.getElementById("demo").innerHTML = "Hello World";
   });
 }
